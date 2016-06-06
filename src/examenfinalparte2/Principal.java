7c2022cb482e4a66c8251dcb7cdb73e880cb8f58
JP <JP@JP>
2016-06-06 09:59:48 +0200

    commit donde explico lo que hice con el contador

--- src/examenfinalparte2/Principal.java
+++ src/examenfinalparte2/Principal.java
@@ -1,4 +1,3 @@
-
 package examenfinalparte2;
 
 import java.io.IOException;
@@ -7,56 +6,56 @@ import java.util.Scanner;
 public class Principal {
 
     public static boolean p = false;
-    
-    public static void main(String arg[]) throws IOException
-        {
-        
-        int ndig=0;
-        
+
+    public static void main(String arg[]) throws IOException {
+
+        int ndig = 0;
+
         System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
-        int dig=(new Scanner(System.in)).nextInt();
-        for(int i = 1; i <= 99999; i++ ){
+        int dig = (new Scanner(System.in)).nextInt();
+        for (int i = 1; i <= 99999; i++) {
             int aux = i;
- 
-            ndig=0;
- 
-            while (aux != 0){
-            aux = aux / 10;
-           ndig++;
-        }
-        
-            
-            
-            if(ndig==dig){
-                   if (i < 4) p = true;
-            else
-            {
-            if (i % 2 == 0) p = false; 
-                else
-                {
-                    int contador1 = 0;  
-                    int i1 = 1; 
-                    int k = (i - 1) / 2; 
-                    if  (k % 2 == 0) k--;  
-             
-                    while(i1 <= k)
-                    {
-                        if (i % i1 == 0) contador1++;
-                            i1 += 2;
-                            if (contador1 == 2) i1 = k + 1;
+
+            ndig = 0;
+
+            while (aux != 0) {
+                aux = aux / 10;
+                ndig++;
+            }
+            //quitamos la variable contador y en el if ponemos la variable ndig ya que el funcionamiento será el mismo
+            if (ndig == dig) {
+                if (i < 4) {
+                    p = true;
+                } else if (i % 2 == 0) {
+                    p = false;
+                } else {
+                    int contador1 = 0;
+                    int i1 = 1;
+                    int k = (i - 1) / 2;
+                    if (k % 2 == 0) {
+                        k--;
                     }
- 
-        if (contador1 == 1) p = true;
-            } 
-        } 
-     
-                if (p == true) 
-                    System.out.println(i);    
+
+                    while (i1 <= k) {
+                        if (i % i1 == 0) {
+                            contador1++;
+                        }
+                        i1 += 2;
+                        if (contador1 == 2) {
+                            i1 = k + 1;
+                        }
+                    }
+
+                    if (contador1 == 1) {
+                        p = true;
+                    }
+                }
+
+                if (p == true) {
+                    System.out.println(i);
+                }
             }
         }
-        }
- 
-       
- 
-    
+    }
+
 }
